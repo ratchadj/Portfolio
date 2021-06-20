48b7d993a9ca6675a2f6fb581fe76e9e374dccfa
Ratchada Jududom <ratchada.jududom@gmail.com>
2021-06-19 22:19:30 -0700

    force siteurl

--- wp-config.php
+++ wp-config.php
@@ -102,3 +102,7 @@ require_once ABSPATH . 'wp-settings.php';
 if (strpos($_SERVER['HTTP_X_FORWARDED_PROTO'], 'https') !== false) {
     $_SERVER['HTTPS'] = 'on';
 }
+
+
+define( 'WP_HOME', 'http://ratchada-jududom.herokuapp.com' );
+define('WP_SITEURL', 'http://ratchada-jududom.herokuapp.com');
