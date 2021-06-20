b20908ea5364d86802cb0edfdf4ff1da9aa2e4ac
Ratchada Jududom <ratchada.jududom@gmail.com>
2021-06-19 21:46:15 -0700

    rename table prefix

--- wp-config.php
+++ wp-config.php
@@ -63,7 +63,7 @@ define( 'NONCE_SALT',       'eeb41827e42ce5cd258c58a9fd5ddcefdfa81601' );
  * You can have multiple installations in one database if you give each
  * a unique prefix. Only numbers, letters, and underscores please!
  */
-$table_prefix = 'wpb_';
+$table_prefix = 'wp_';
 
 /**
  * For developers: WordPress debugging mode.
